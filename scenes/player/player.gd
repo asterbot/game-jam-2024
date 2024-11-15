2b4974d1107aa5d60b845fd11bbad666753adb00
asterbot <arjunsodhi7@gmail.com>
2024-11-15 00:03:36 -0500

    player movement animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,11 +2,11 @@ extends CharacterBody2D
 
 
 const H_VEL_DELTA = 25
-const MAX_H_VEL = 520.0
+const MAX_H_VEL = 600.0
 const MAX_V_VEL = 4000.0
 const JUMP_VELOCITY = -800.0
 const EXTRA_JUMP_VELOCITY = -600.0
-const FRICTION = 50
+const FRICTION = 80
 
 const EXTRA_JUMPS = 10
 var extra_jumps : int = 0
@@ -35,7 +35,6 @@ func get_platform_normal():
 	return Vector2.ZERO
 
 
-
 func _physics_process(delta: float) -> void:
 	
 	# apply gravity each frame, apply move immediately
@@ -56,7 +55,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x += direction * H_VEL_DELTA
 	if direction == 0 or velocity.x < -MAX_H_VEL or velocity.x > MAX_H_VEL:
 		velocity.x = move_toward(velocity.x, 0, FRICTION)
+	$PlayerImage.flip_h = (direction<0)
 	
+
 	# control vertical movement
 	if Input.is_action_just_pressed("up") and (is_on_floor() or near_floor()):
 		velocity.y = JUMP_VELOCITY
@@ -69,4 +70,4 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("speed"):
 		velocity = 2*Vector2(-3000, -500)
 	
-	print(velocity)
+	#print(velocity)
