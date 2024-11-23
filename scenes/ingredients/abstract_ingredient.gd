fe1756f7887a2ec0af13ea26a385813562430b68
InvertedCarrot <dyuan9378@gmail.com>
2024-11-22 23:41:29 -0500

    added display name for berry

--- scenes/ingredients/abstract_ingredient.gd
+++ scenes/ingredients/abstract_ingredient.gd
@@ -3,17 +3,19 @@ extends RigidBody2D
 class_name Ingredient
 
 const SPEED_THRESHOLD: int = 2000
-
+var display_name = ""
 
 
 func _ready() -> void:
-	pass
+	$Label.visible = true
+	$Label.text = display_name
 
 func _process(_delta) -> void:
 	if linear_velocity.length_squared() > 0:
 		gravity_scale = 1
 		if $DespawnTimer.is_stopped():
 			$DespawnTimer.start()
+	$Label.rotation = -rotation
 
 func _integrate_forces(state) -> void:
 	# set upper bound for ingredient velocity
--- scenes/ingredients/berry.gd
+++ scenes/ingredients/berry.gd
@@ -1,3 +1,7 @@
 extends Ingredient
 
 var ingredient_name: String = "berries"
+
+func _ready():
+	display_name = "winged berry"
+	super()
