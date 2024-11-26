1ce5035738ad1e6b61b9425c28c3ed8a819e2057
Arjun <62688208+asterbot@users.noreply.github.com>
2024-11-26 14:15:10 -0500

    Ghost enemy (#14)
    * virtual functions
    
    * sprout idle pass
    
    * Fixed some bugs with enemy to ensure interaction with items
    
    * new ghost attack type

--- scenes/enemies/sprout.gd
+++ scenes/enemies/sprout.gd
@@ -1,25 +1,19 @@
 extends AbstractEnemy
 
-var can_attack = true
-var dead = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Timer.set_wait_time(randf_range(3, 4))
-
+func idle(_delta):
+	pass
 
 func hit():
 	dead = true
 	$AnimationPlayer.play("dead")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if not dead:
-		if can_attack:
-			$AnimationPlayer.play("attack")
-			$AnimationPlayer.queue("idle")
-			can_attack = false
-			$Timer.start()
+func attack():
+	$AnimationPlayer.play("attack")
+	$AnimationPlayer.queue("idle")
 
-func _on_timer_timeout() -> void:
-	can_attack = true
+func _ready() -> void:
+	periodic_attack = true
+	super()
+
+func _process(_delta: float) -> void:
+	super(_delta)
