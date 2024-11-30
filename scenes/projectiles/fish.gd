767bf82d31bc0c04d3a4e2571b700651f0a03ca2
asterbot <arjunsodhi7@gmail.com>
2024-11-30 17:13:17 -0500

    minor fixes

--- scenes/projectiles/fish.gd
+++ scenes/projectiles/fish.gd
@@ -8,3 +8,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	super(delta)
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	super(state)
+	if rad_to_deg(rotation) > 90 and rad_to_deg(rotation) < 270:
+			$Sprite2D.flip_v = true
