d6b6f6d15e08ce50cb181f9c06f41a6776f3690e
InvertedCarrot <dyuan9378@gmail.com>
2024-12-01 10:50:33 -0500

    level two started

--- scenes/levels/cave_entrance.gd
+++ scenes/levels/cave_entrance.gd
@@ -3,18 +3,18 @@ extends Node2D
 var can_enter = false
 
 func _ready():
-	$Key/Sprite2D.set_texture(preload("res://assets/keys/letters/q_key.png"))
+	$Key/Sprite2D.set_texture(preload("res://assets/keys/letters/e_key.png"))
 	$Key.visible = false
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if can_enter and Input.is_action_just_pressed("interact") and not Globals.cookbook_open and not Globals.dialogue_open:
 		Transition.change_scene("res://scenes/levels/level_inside/level_inside.tscn")
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	can_enter = true
 	$Key.visible = true
 	
-func _on_area_2d_body_exited(body: Node2D) -> void:
+func _on_area_2d_body_exited(_body: Node2D) -> void:
 	can_enter = false
 	$Key.visible = false
